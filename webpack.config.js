304c269caae1d58c2c6b7826861ab6601755c1de
李朋 <lipeng@1fangxin.cn>
2018-04-04 16:43:03 +0800

    打包压缩编译

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
 const webpackConfig = require('./node_modules/laravel-mix/setup/webpack.config')
 const vuxLoader = require('vux-loader')
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+
 
 module.exports = vuxLoader.merge(webpackConfig, {
   options: {},
   plugins: [
     { name: 'vux-ui' },
+    new ParallelUglifyPlugin({
+      cacheDir: '.cache/',
+      uglifyJS:{
+        output: {
+          comments: false
+        },
+        compress: {
+          warnings: true
+        }
+      }
+    }),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     'NODE_ENV': JSON.stringify('production')
+    //   }
+    // }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     warnings: false
+    //   }
+    // })
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // })
     // {
     //   name: 'less-theme',
     //   path: './resources/assets/wx/css/theme.less'
