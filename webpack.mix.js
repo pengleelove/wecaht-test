af79019e379e3c5afa981f5ecef341b1a81c1a45
李朋 <lipeng@1fangxin.cn>
2018-04-04 15:03:13 +0800

    更改显示问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
+  .less('~vux/src/styles/reset.less','publi/css/reset.css');
