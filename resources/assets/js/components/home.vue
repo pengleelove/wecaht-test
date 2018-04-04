b4e2ec220bdb136c1973182c166de954c2ad5a9c
李朋 <lipeng@1fangxin.cn>
2018-04-04 20:08:44 +0800

    增加音乐

--- resources/assets/js/components/home.vue
+++ resources/assets/js/components/home.vue
@@ -3,8 +3,16 @@
        <!--<group>-->
            <!--<cell title="title" value="value">123333</cell>-->
        <!--</group>-->
-       <router-link to="/test">go to test</router-link>
-       欢迎来到 Raybon 的Code 世界
+       <div>
+           <iframe frameborder="yes" border="4" marginwidth="0" marginheight="0" width=330 height=86
+                   src="//music.163.com/outchain/player?type=2&id=27949692&auto=1&height=66"></iframe>
+       </div>
+       <div>
+           <router-link to="/test">go to test</router-link>
+           欢迎来到 Raybon 的Code 世界
+       </div>
+       <!--<embed src="//music.163.com/style/swf/widget.swf?sid=27949692&type=2&auto=1&width=320&height=66" width="340" height="86"  allowNetworking="all"></embed>-->
+
    </div>
 </template>
 
