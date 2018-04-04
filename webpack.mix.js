bce6146a8e0142a7c478f00b3b712d4aff984921
李朋 <lipeng@1fangxin.cn>
2018-04-04 15:09:45 +0800

    提交

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
-  .less('~vux/src/styles/reset.less','publi/css/reset.css');
+   .sass('resources/assets/sass/app.scss', 'public/css/app.css');
