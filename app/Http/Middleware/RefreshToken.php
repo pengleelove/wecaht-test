8ac3f258eb01b6593f12e8a05a3894116ba69467
李朋 <lipeng@1fangxin.cn>
2018-04-12 17:51:10 +0800

    token

--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
@@ -30,10 +32,19 @@ class RefreshToken extends BaseMiddleware
                 return $next($request);
             }
             throw new UnauthorizedHttpException('jwt-auth', '未登录');
-        } catch (TokenExpiredException $exception) {
+        } catch (TokenExpiredException $except) {
+            //     如果此处 捕获到了 token 过期所抛出的 TokenExpiredException 异常 ，   我们在这里需要做的就是刷新该用户的 token 并将它添加到响应头中
+            try {
+                //     刷新用户token
+                $token = $this->auth->refresh();
+                //     使用一次性登录 以保证此次请求的成功
+                Auth::guard('api')->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()
+                    ->toPlainArray()['sub']);
+            } catch (JWTException $exception) {
+                //      如果捕获到此异常 即代表 refresh 也过期了，用户无法刷新令牌，需要重新登录了
+                throw  new  UnauthorizedHttpException('jwt-auth', $exception->getMessage());
+            }
+            return $this->setAuthenticationHeader($next($request), $token);
         }
-
-
-        return $next($request);
     }
 }
