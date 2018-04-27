46c143d253228e28a14ad1b83fdf386a825e6d5f
李朋 <lipeng@1fangxin.cn>
2018-04-27 16:23:18 +0800

    增加授权登录 代码格式化

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::group([], function () {
 });
+Route::get('/', 'HomeController@index');
+//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 
 Route::get('/test', function () {
     return '测试成功';
 });
+Auth::routes();
+
+//Route::get('/show-register', 'Auth\RegisterController@showRegistrationForm')->name('show.register');
+//Route::post('/register', 'Auth\RegisterController@create')->name('register');
+//Route::get('/password-request', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//Route::get('/email', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.email');
