3644619691189d5f2bda0e9ef66e180a984fc9e7
李朋 <lipeng@1fangxin.cn>
2018-04-28 09:33:32 +0800

    调整路由

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::group([], function () {
-});
 Route::get('/', 'HomeController@index');
-//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 
 Route::get('/test', function () {
     return '测试成功';
 });
 Auth::routes();
 
-//Route::get('/show-register', 'Auth\RegisterController@showRegistrationForm')->name('show.register');
-//Route::post('/register', 'Auth\RegisterController@create')->name('register');
-//Route::get('/password-request', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-//Route::get('/email', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.email');
