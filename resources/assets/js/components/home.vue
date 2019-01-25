64c9eba7dab88da01d1bc7fca0379496bf01f730
李朋 <lipeng@1fangxin.cn>
2019-01-25 09:45:13 +0800

    放开音乐管理

--- resources/assets/js/components/home.vue
+++ resources/assets/js/components/home.vue
@@ -5,18 +5,17 @@
        <!--</group>-->
        <!--http://cloud.video.taobao.com/play/u/1748679248/p/2/e/3/t/1/70931570.m3u8-->
        <div>
-<!--http://music.163.com/#/m/song?id=28273424-->
-           <!--http://music.163.com/#/m/song?id=37808667-->
-           <!--<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=33211676&auto=1&height=66"></iframe>-->
-           <!--<iframe frameborder="yes" border="4"  marginwidth="0" marginheight="0" width=330 height=86-->
-                   <!--src="http://cloud.video.taobao.com/play/u/1748679248/p/2/e/3/t/1/70931570.m3u8"></iframe>-->
-           <!--<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86-->
-                   <!--src="//music.163.com/outchain/player?type=2&id=5271522&auto=1&height=66"></iframe>-->
+
+           <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=33211676&auto=1&height=66"></iframe>
+           <iframe frameborder="yes" border="4"  marginwidth="0" marginheight="0" width=330 height=86
+                   src="http://cloud.video.taobao.com/play/u/1748679248/p/2/e/3/t/1/70931570.m3u8"></iframe>
+           <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86
+                   src="//music.163.com/outchain/player?type=2&id=5271522&auto=1&height=66"></iframe>
 
 
        </div>
        <div>
-           <router-link to="/test">go to test</router-link>
+           <router-link to="/">go to test</router-link>
            欢迎来到 Raybon 的Code 世界
        </div>
        <!--<embed src="//music.163.com/style/swf/widget.swf?sid=27949692&type=2&auto=1&width=320&height=66" width="340" height="86"  allowNetworking="all"></embed>-->
